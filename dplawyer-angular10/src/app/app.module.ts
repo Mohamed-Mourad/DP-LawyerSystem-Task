f00acf679bec21a7ebf50ca0a67717e92a392d6e
Mohamed Mourad <mohamed.m_2000@outlook.com>
2024-11-16 15:11:58 +0200

    angular 10 app module

--- dplawyer-angular10/src/app/app.module.ts
+++ dplawyer-angular10/src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+  import { BrowserModule } from '@angular/platform-browser';
+  import { NgModule } from '@angular/core';
+  import { RouterModule, Routes } from '@angular/router';
+  import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+  import { AppRoutingModule } from './app-routing.module';
+  import { AppComponent } from './app.component';
+  import { AppBarComponent } from './shared/app-bar/app-bar.component';
+  import { NotificationPageComponent } from './pages/notification-page/notification-page.component';
+  import { SubscriptionService } from './services/subscription-service/subscription.service';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+  const routes: Routes = [
+    { path: '', component: NotificationPageComponent },
+  ];
+
+  @NgModule({
+    declarations: [
+      AppComponent,
+      AppBarComponent,
+      NotificationPageComponent,
+    ],
+    imports: [
+      BrowserModule,
+      AppRoutingModule,
+      RouterModule.forRoot(routes),
+    ],
+    providers: [
+      SubscriptionService
+    ],
+    bootstrap: [AppComponent]
+  })
+  export class AppModule { }
